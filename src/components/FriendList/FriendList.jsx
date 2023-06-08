926715d12d2bb717af9e7da4b7a9528c50fb7f01
OleksiiShevchenko1 <117725999+OleksiiShevchenko1@users.noreply.github.com>
2023-06-08 15:49:38 +0300

    finish

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FriendList.module.scss';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      isOnline: PropTypes.bool,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
+};
+
+export default FriendList;
